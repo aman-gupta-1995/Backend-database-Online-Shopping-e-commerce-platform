4b98dcd779cd29c7502fa25cd7277939aad910ea
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-03-10 12:40:55 +0530

    Update add_product.php

--- admin/add_product.php
+++ admin/add_product.php
@@ -11,7 +11,7 @@ $details=$_POST['details'];
 $price=$_POST['price'];
 $c_price=$_POST['c_price'];  
 $product_type=$_POST['product_type'];
-$brand=$_POST['brand']; 
+$brand=$_POST['brand'];  
 $tags=$_POST['tags'];
 
 //picture coding 
