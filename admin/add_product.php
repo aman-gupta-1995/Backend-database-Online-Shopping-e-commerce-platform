8aacf0ce0ee8f9e6a2e3ded68281c75472c0eda2
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-03-10 12:40:32 +0530

    Update add_product.php

--- admin/add_product.php
+++ admin/add_product.php
@@ -9,7 +9,7 @@ if(isset($_POST['submit']))
 $product_name=$_POST['product_name'];   
 $details=$_POST['details']; 
 $price=$_POST['price'];
-$c_price=$_POST['c_price']; 
+$c_price=$_POST['c_price'];  
 $product_type=$_POST['product_type'];
 $brand=$_POST['brand'];
 $tags=$_POST['tags'];
