ec937b9b67e742e8c44a4a9b3f7e14cf7946d385
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-03-10 12:41:11 +0530

    Update add_product.php

--- admin/add_product.php
+++ admin/add_product.php
@@ -12,7 +12,7 @@ $price=$_POST['price'];
 $c_price=$_POST['c_price'];  
 $product_type=$_POST['product_type'];
 $brand=$_POST['brand'];  
-$tags=$_POST['tags'];
+$tags=$_POST['tags']; 
 
 //picture coding 
 $picture_name=$_FILES['picture']['name'];
