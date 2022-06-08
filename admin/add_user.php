27861e68216605c4fa9f2cd64bf7b66447cc81a3
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-06-08 16:54:00 +0530

    Update add_user.php

--- admin/add_user.php
+++ admin/add_user.php
@@ -4,7 +4,7 @@ include("../db.php");
 
 if(isset($_POST['btn_save'])) 
 { 
-$first_name=$_POST['first_name'];
+$first_name=$_POST['first_name']; 
 $last_name=$_POST['last_name'];
 $email=$_POST['email'];
 $user_password=$_POST['user_password'];
