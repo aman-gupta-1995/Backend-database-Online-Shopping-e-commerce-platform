0d3f98d74b17b758a72036829eea69983a93bd3c
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-06-08 16:45:53 +0530

    Update header.php

--- admin/include/header.php
+++ admin/include/header.php
@@ -3,7 +3,7 @@
 	<div class="navbar-header">
 			<button type="button" class="navbar-toggle navbar-toggle-sidebar collapsed">
 			MENU
-			</button>
+			</button> 
 			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 				<span class="sr-only">Toggle navigation</span>
 				<span class="icon-bar"></span>   
